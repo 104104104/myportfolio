fbf0c8385dfd89144372b838882b3b5624c618b2
{104104104} <{rz344788@yahoo.co.jp}>
2019-09-08 17:48:18 +0900

    always fixed menubar

--- static/myjs/move.js
+++ static/myjs/move.js
@@ -1,54 +1,65 @@
 //NowLoadingのプラグイン
-Pace.on('done', function () {
-	$('.wrap').addClass('do');
-	$('.wrap').hide().fadeIn();
+Pace.on('done', function() {
+    $('.wrap').addClass('do');
+    $('.wrap').hide().fadeIn();
 });
 //NowLoadingここまで
 
+
 //マップをレスポンシブにするためのjqueryプラグイン
-$(function () {
-	$('img[usemap]').rwdImageMaps();
+$(function() {
+    $('img[usemap]').rwdImageMaps();
 });
 //マップレスポンシブここまで
 
+//トップページを動かすためのjs
+//吉田信将のポートフォリオ　を固定した上で、棒を動かす
+const newtopbarposi = $('#topchar1id').offset().top + $('#topchar1id').height();
+$('#topbarid').offset({
+    top: newtopbarposi
+});
+//トップページを動かすためのjsここまで
+
+
+
+//メニューバーを固定するためのjs
 var posi_top, wih_half, current_view;
+var menupos;
 
-$(window).on('load', function () {
-	var wih = window.innerHeight;
-	var wih_half = wih / 2;
-	current_view = wih_half;
-	set_posi();
+//読み込み終わりに、座標取得&関数実行
+$(window).on('load', function() {
+
+    current_view = $(this).scrollTop();
+    menupos = $('#menuid').offset();
+    set_posi();
+
 });
+//ウィンドウリサイズ時に、座標取得&関数実行
+$(window).resize(function() {
 
-$(window).resize(function () {
+    current_view = $(this).scrollTop();
+    menupos = $('#menuid').offset();
+    set_posi();
 
-	var wih = window.innerHeight;
-	var wih_half = wih / 2;
-	current_view = wih_half;
-	set_posi();
 });
+//スクロール時に、座標取得&関数実行
+$(window).scroll(function() {
 
-$(window).scroll(function () {
-
-	var wih = window.innerHeight;
-	var wih_half = wih / 2;
-	current_view = $(this).scrollTop();
-	set_posi();
+    current_view = $(this).scrollTop();
+    menupos = $('#menuid').offset();
+    set_posi();
 
 });
 
 function set_posi() {
 
-	$('.notAbsolute').each(function () {
-		var posi = $(this).offset();
-		posi_top = posi.top;
-
-		//戻ればなんども
-		if (current_view > posi_top + this.height * (8 / 9)) {
-			$('.fixed-menu').addClass('do');
-		} else {
-			$('.fixed-menu').removeClass('do');
-		}
-	});
-
+    //戻ればなんども
+    if (current_view > menupos.top) {
+        console.log(current_view, menupos.top);
+        $('#menuid2').addClass('domenu');
+        //console.log("over!");
+    } else {
+        $('#menuid2').removeClass('domenu');
+        //console.log("not over!");
+    }
 }
