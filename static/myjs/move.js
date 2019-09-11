423d8d122ce5ffc51b9b02a268eb327d586a17b5
{104104104} <{rz344788@yahoo.co.jp}>
2019-09-11 20:50:09 +0900

    読み込んでから表示をオフに　

--- static/myjs/move.js
+++ static/myjs/move.js
@@ -1,8 +1,10 @@
 //全部読み込んでから、表示する
+/*挙動が遅いので、オフに
 $(window).on('load', function() {
     $('.wrap').addClass('do');
     $('.wrap').hide().fadeIn();
 });
+*/
 
 //メニューバーのしたを開けるためのjquery
 $('#spaceForMenuid').css('margin-top', String($('#normalmenu').height() + 20) + "px");
